c5a66e921babbea097499bbbb76567e65fc08c47
Thuy Vy Tran <110182087+tvytran@users.noreply.github.com>
2026-08-14 22:11:05 -0400

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        dic = {}
+        for a,b in prerequisites:
+            if a not in dic:
+                dic[a] = []
+            dic[a].append(b)
+        
+        visited = set()
+        def dfs( i):
+            if i not in dic or len(dic[i]) == 0:
+                return True
+            if i in visited:
+                return False
+            
+            visited.add(i)
+            for c in dic[i]:
+                if dfs(c) == False:
+                    return False
+            visited.remove(i)
+            dic[i]=[]
+            return True
+        print(dic)
+        for course, pre in dic.items():
+            if dfs(course) == False:
+                return False
+        return True
+
+         
+
+
+        
+        
